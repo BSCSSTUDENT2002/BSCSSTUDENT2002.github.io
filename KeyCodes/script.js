dcf3a310e934aaec318241f412842c2b52e92bec
AHMAD <ahmadrazasiddiqi5@gmail.com>
2024-08-02 08:52:32 +0500

    First Commit

---
+++ KeyCodes/script.js
@@ -0,0 +1,28 @@
+var flag=0
+$('body').keydown((e)=>{
+if(flag==0){
+    $(".main").remove()
+    $(".container").append("<div class='append'><div id='1'></div><div id='2'></div><div id='3'></div></div>")
+    $('#1').text(e.key);
+    $('#2').text(e.keyCode);
+    $('#3').text(e.code);
+    flag=1
+}
+else app(e)
+$('#1, #2, #3').css({
+    'color': 'gray',
+    'border': '2px solid grey',
+    'height': '50px',
+    'width':'150px',
+    'background-color': '#f1faee',
+    // 'text-direct'
+});
+
+})
+function app(e){
+    $('#1').text(e.key);
+$('#2').text(e.keyCode);
+$('#3').text(e.code);
+flag=1;
+}
+
